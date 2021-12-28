a3499bf6145bd025aa6973f21767ba9783d90bdc
Pawel Bak <pawelbak300589@gmail.com>
2021-12-28 17:59:50 +0000

    Add portfolio template and update endpoints to use new template pages

--- server.py
+++ server.py
@@ -5,25 +5,10 @@ print(__name__)
 
 
 @app.route("/")
-def hello_world():
+def my_home():
     return render_template('index.html')
 
 
-@app.route("/<username>")
-def hi(username=None):
-    return render_template('hi.html', name=username)
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-
-@app.route("/blog")
-def blog():
-    return "<p>this is a blog page!</p>"
-
-
-@app.route("/blog/<int:year>/<post_title>")
-def blog_post(year=None, post_title=None):
-    return render_template('blog.html', year=year, title=post_title)
+@app.route("/<string:page_name>")
+def html_page(page_name):
+    return render_template(f'{page_name}.html')
