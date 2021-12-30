3d4b5f902bc3865269e0fdad456b4b7b253198a0
Pawel Bak <pawelbak300589@gmail.com>
2021-12-30 13:08:06 +0000

    Create database as a text file

--- server.py
+++ server.py
@@ -14,11 +14,19 @@ def html_page(page_name):
     return render_template(f'{page_name}.html')
 
 
+def write_to_file(data):
+    with open('./database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email},{subject},{message}')
+
+
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
         return redirect('/thankyou')
     else:
         return 'Something went wrong. Please try again!'
